cb47a63229bf04fc36a6bd9c12c82dfac4886390
VMLujanJr <vmlujanjr@outlook.com>
2022-06-27 10:42:21 -0500

    recent updates

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const {
     addComment,
-    removeComment
+    removeComment,
+    addReply,
+    removeReply
 } = require('../../controllers/comment-controller.js');
 
 
@@ -13,6 +15,22 @@ router
 // /api/comments/<pizzaId>/<commentId>
 router
     .route('/:pizzaId/:commentId')
+    .put(addReply)
     .delete(removeComment);
+
+// /api/<pizzaId>/<commentId>/<replyId>
+/* created new route because you need the id of the individual reply,
+    not just its parent
+*/
+
+/*
+It's kind of like saying,
+"Go to this pizza,
+then look at this particular comment,
+then delete this one reply."
+*/
+router
+    .route('/:pizzaId/:commentId/:replyId')
+    .delete(removeReply);
 
 module.exports = router;
